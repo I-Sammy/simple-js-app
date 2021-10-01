e49b5c0d1ac2b0f0c6afc7ba204516ff3b007e17
I-Sammy <samiaislam@outlook.com>
2021-10-02 04:07:04 +1000

    Update scripts.js
    added objects to array

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,43 @@
-alert('Hello World!');
+//Declare a blank array to contain pokemon list
+let pokemonList = [];
 
-let favouriteFood;
-favouriteFood = 'Thai';
+//Adding objects to the array
+let pikachu = {
+   name: 'pikachu'
+  ,height: 0.4
+  ,weight: 6.0
+  ,types: ['electric']
+};
+let charmander = {
+  name: 'Charmander'
+ ,height: 0.6
+ ,weight: 8.5
+ ,types: ['fire']
+};
+let bulbasaur = {
+  name: 'Bulbasaur'
+ ,height: 0.7
+ ,weight: 6.9
+ ,types: ['grass','poison']
+};
+let butterfree = {
+  name: 'Butterfree'
+ ,height: 1.1
+ ,weight: 32.0
+ ,types: ['bug','flying']
+};
+let igglybuff = {
+  name: 'Igglybuff'
+ ,height: 0.3
+ ,weight: 1.0
+ ,types: ['fairy','normal']
+};
 
-document.write("Favourite Food: " + favouriteFood);
+//Assigning the objects to the array
+pokemonList = [
+   pikachu
+  ,charmander
+  ,bulbasaur
+  ,butterfree
+  ,igglybuff
+];
